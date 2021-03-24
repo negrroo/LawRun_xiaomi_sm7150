a14900b51150cf82ea66a7782e28863b19bce37f
Karou <71267804+Sleppyiecat@users.noreply.github.com>
2021-03-24 19:05:15 +0100

    buildme.sh: make it look better

--- buildme.sh
+++ buildme.sh
@@ -1,4 +1,4 @@
-   git config --global user.name "Sleppyiecat"
+            git config --global user.name "Sleppyiecat"
             git config --global user.email "mruczek70@gmail.com"         
             sudo -E apt-get -qq update
             sudo -E apt-get -qq install bc build-essential zip libstdc++6 git wget gcc clang libssl-dev repo rsync flex curl ccache bison aria2
