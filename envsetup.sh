c666e2c8b78779eaaf6e56014ec9d762824d5385
Karou <71267804+Sleppyiecat@users.noreply.github.com>
2021-04-06 18:37:09 +0200

    envsetup.sh: new line time

--- envsetup.sh
+++ envsetup.sh
@@ -4,3 +4,4 @@
             sudo -E apt-get -qq update
             sudo -E apt-get -qq install bc build-essential zip libstdc++6 git wget gcc clang libssl-dev repo rsync flex curl ccache bison aria2
             git clone https://github.com/kdrag0n/proton-clang --depth=1 -b master toolchain
+
